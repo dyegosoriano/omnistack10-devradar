57f905995d8fe535033aa427d368052387e559b6
Dyego Soriano <dyegodf2@gmail.com>
2020-01-20 13:01:08 -0300

    conexão com banco de dados

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://omnistack10:omnistack10@omnistack-4xggp.mongodb.net/test?retryWrites=true&w=majority', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+})
+
+// Métodos: GET, POST, PUT , DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: req.query (filtros, ordenação, paginação...)
+// Route Params: req.params (Identificar um recurso na alteração ouremoção)
+// Body: req.body (Dados para a criação ou alteração de um registro)
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   return res.json({ mensagem: 'Olá mundo' })
