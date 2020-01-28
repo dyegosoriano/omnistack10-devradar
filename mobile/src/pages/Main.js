a8fb36500357c22da928ca103df6c0f4f4e554bf
Dyego Soriano <dyegodf2@gmail.com>
2020-01-28 15:55:36 -0300

    formulario de busca adicionado

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function Main ({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null)
@@ -33,20 +34,36 @@ export default function Main ({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{latitude: -12.992635, longitude: -38.5057234}}>
-        <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/4140145?s=400&u=d2c245d62a73752fa034546e277d637cca37dc59&v=4' }}/>
-        <Callout onPress={() => {
-          navigation.navigate('Profile', { github_username: 'dyegodf2' })
-        }}>
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Dyego Soriano</Text>
-            <Text style={styles.devBio}>Biografia</Text>
-            <Text style={styles.devTechs}>Node.JS, ReactJS, React Native</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{latitude: -12.992635, longitude: -38.5057234}}>
+          <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/4140145?s=400&u=d2c245d62a73752fa034546e277d637cca37dc59&v=4' }}/>
+          <Callout onPress={() => {
+            navigation.navigate('Profile', { github_username: 'dyegodf2' })
+          }}>
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Dyego Soriano</Text>
+              <Text style={styles.devBio}>Biografia</Text>
+              <Text style={styles.devTechs}>Node.JS, ReactJS, React Native</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder='Buscar devs por techs...'
+          placeholderTextColor='#999'
+          autoCapitalize='words'
+          autoCorrect={false}
+        />
+
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name='my-location' size={20} color='#fff'/>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -80,6 +97,42 @@ const styles = StyleSheet.create({
 
   devTechs: {
     marginTop: 5,
-  }
+  },
+
+  searchForm: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row'
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#fff',
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 2,
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#8e4dff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15
+  },
 
 })
