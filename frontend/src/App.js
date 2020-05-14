01afe25007e224789bfee4016d574a155cbded3b
Dyego Soriano <dyegodf2@gmail.com>
2020-05-14 18:14:10 -0300

    refactor code front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,46 @@
-import React, {useEffect, useState} from 'react';
-import api from './services/api'
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
-import './global.css'
-import './App.css'
-import './Sidebar.css'
-import './Main.css'
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
-import DevForm from './components/DevForm'
-import DevItem from './components/DevItem'
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App() {
-  const [devs, setDevs] = useState([])
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    async function loadDevs () {
-      const response = await api.get('/devs')
-      setDevs(response.data)
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
     }
 
-    loadDevs()
-  }, [])
+    loadDevs();
+  }, []);
 
-  async function handleAddDev (data) {
-    const response = await api.post('/devs', data)
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
 
-    setDevs([...devs, response.data])
+    setDevs([...devs, response.data]);
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <DevForm onSubmit={handleAddDev}/>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
-      <ul>
-        {devs.map(dev => (
-          <DevItem key={dev._id} dev={dev}/>
-        ))}
-      </ul>
-    </main>
+        <ul>
+          {devs.map((dev) => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
--- frontend/src/components/DevForm/index.js
+++ frontend/src/components/DevForm/index.js
@@ -1,22 +1,20 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './styles.css';
 
-export default function DevForm({onSubmit}) {
-  const [github_username, setGithubUserName] = useState('')
-  const [techs, setTechs] = useState('')
-  const [latitude, setLatitude] = useState('')
-  const [longitude, setLongitude] = useState('')
+export default function DevForm({ onSubmit }) {
+  const [github_username, setGithubUserName] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log(position);
+        const { latitude, longitude } = position.coords;
 
-        const {latitude, longitude} = position.coords
-
-        setLatitude(latitude)
-        setLongitude(longitude)
+        setLatitude(latitude);
+        setLongitude(longitude);
       },
       (err) => {
         console.log(err);
@@ -24,72 +22,74 @@ export default function DevForm({onSubmit}) {
       {
         timeout: 30000,
       }
-    )
-  }, [])
+    );
+  }, []);
 
-  async function handleSubmit (event) {
-    event.preventDefault()
+  async function handleSubmit(event) {
+    event.preventDefault();
 
     await onSubmit({
       github_username,
       techs,
       latitude,
-      longitude
-    })
+      longitude,
+    });
 
-    setGithubUserName('')
-    setTechs('')
+    setGithubUserName('');
+    setTechs('');
   }
 
   return (
     <form onSubmit={handleSubmit}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input
-              name="github_username"
-              id="github_username"
-              required
-              value={github_username}
-              onChange={event => setGithubUserName(event.target.value)}
-              />
-          </div>
+      <div className="input-block">
+        <label htmlFor="github_username">Usuário do Github</label>
+        <input
+          name="github_username"
+          id="github_username"
+          required
+          value={github_username}
+          onChange={(event) => setGithubUserName(event.target.value)}
+        />
+      </div>
 
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={event => setTechs(event.target.value)}
-              />
-          </div>
+      <div className="input-block">
+        <label htmlFor="techs">Tecnologias</label>
+        <input
+          name="techs"
+          id="techs"
+          required
+          value={techs}
+          onChange={(event) => setTechs(event.target.value)}
+        />
+      </div>
 
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                required value={latitude}
-                onChange={event => setLatitude(event.target.value)}
-              />
-            </div>
+      <div className="input-group">
+        <div className="input-block">
+          <label htmlFor="latitude">Latitude</label>
+          <input
+            type="number"
+            name="latitude"
+            id="latitude"
+            required
+            value={latitude}
+            onChange={(event) => setLatitude(event.target.value)}
+          />
+        </div>
 
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required value={longitude}
-                onChange={event => setLongitude(event.target.value)}
-              />
-            </div>
-          </div>
+        <div className="input-block">
+          <label htmlFor="longitude">Longitude</label>
+          <input
+            type="number"
+            name="longitude"
+            id="longitude"
+            required
+            value={longitude}
+            onChange={(event) => setLongitude(event.target.value)}
+          />
+        </div>
+      </div>
 
-          <button type="submit">Salvar</button>
-        </form>
+      <button type="submit">Salvar</button>
+    </form>
   );
 }
